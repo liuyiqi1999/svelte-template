7ef1636d568a40969069846e9c0d7d9d70d2b8cc
刘亦奇 <liuyiqi1999@163.com>
2022-05-17 23:59:14 +0800

    feat: unocss, darkmode

--- src/App.svelte
+++ src/App.svelte
@@ -1,82 +1,42 @@
 <script>
   import Header from "./lib/header/Header.svelte";
-  import { ThemeWrapper } from 'svelte-themer'
-  import { themes } from "./themes";
 </script>
 
-<ThemeWrapper themes="{themes}">
 <Header />
-<main>
-  
+<main max-w-5xl text-center mx-auto>
   <h1>Hello world!</h1>
-
   <p>Start your svelte project! </p>
-
 </main>
-<footer>Powered by <a href="https://svelte.dev">Svelte</a></footer>
-</ThemeWrapper>
+<footer text-sm c-gray-400 w-full text-center>Powered by <a href="https://svelte.dev">Svelte</a></footer>
 
-<style>
-  :global(html) {
-    background-color: var(--theme-colors-background, initial);
-    color: var(--theme-colors-text, initial);
+<style lang="scss">
+  :global(html, body) {
+    margin: 0;
+    padding: 0;
   }
 
-  :root {
+  :global(html) {
+    color: rgb(34, 34, 34);
+    background-color: rgb(246, 246, 246);
+  }
+
+  :global(html.dark) {
+    color: rgb(246, 246, 246);
+    background-color: rgb(34, 34, 34);
+  }
+
+  :global(#app) {
     font-family: Arial, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
       Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
   a {
-    color: var(--theme-colors-primary)
+    color: #ff3e00;
   }
-
-  a:hover {
-    color: var(--theme-colors-accent)
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  footer {
-    font-size: 0.9em;
-    color: var(--theme-colors-secondary);
-    width: 100%;
-		text-align: center;
-	}
 
 	@media (min-width: 480px) {
 		footer {
 			padding: 40px 0;
 		}
 	}
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,32 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import Icons from 'unplugin-icons/vite'
+import Unocss from 'unocss/vite'
+import { presetAttributify, presetUno, presetIcons } from 'unocss'
+import { extractorSvelte } from '@unocss/core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), Icons({compiler: 'svelte'}),]
+  plugins: [
+    Unocss({
+      extractors: [extractorSvelte],
+      presets: [
+        presetAttributify({ /* preset options */}),
+        presetUno(),
+        presetIcons({
+          extraProperties: {
+            'display': 'inline-block',
+            'vertical-align': 'middle',
+            'min-width': '1em'
+          },
+        }),
+      ],
+      rules: [
+        ['mono', {'font-family': `'Major Mono Display', monospace;`}],
+      ],
+      theme: {
+        colors: {}
+      }
+    }),
+    svelte(),
+  ]
 })
